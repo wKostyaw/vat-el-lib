49f2aef30ba4683f335b937c9002424584f113b2
wKostyaw <49492399+wKostyaw@users.noreply.github.com>
2019-05-31 16:29:47 +0300

    Утренние фиксы и объединенная админская навигация

--- CustomizeSlider.php
+++ CustomizeSlider.php
@@ -92,7 +92,7 @@
 		<meta charset="utf-8">
 		<title>Настройки слайдеров</title>
 		<link rel="stylesheet" type="text/css" href="Css/AdminPage.css">
-		<link rel="stylesheet" type="text/css" href="Css/SliderCustomize.css">
+		<link rel="stylesheet" type="text/css" href="Css/sliderCustomize.css">
 		<script src="Js/JQuerry.js" type="text/javascript"></script>
 		<script src="Js/SliderCustomize.js" type="text/javascript"></script>
 		<script src="Js/AddOneMore.js" type="text/javascript"></script>
@@ -100,20 +100,7 @@
 	<body>
 		<div class="Wrapper">
 			<div class="Sidebar">
-				<div class="LogoContainer">
-					<img src="Img/WorkInProgress.png" class="Logo">
-				</div>
-					<ul class="AdminLinks">
-						<li class="AdminLinkBox"><a href="mainpage.php" class="AdminLink">На сайт</a></li>
-						<li class="AdminLinkBox"><a href="AddUserForm.php" class="AdminLink">Добавить пользователя</a></li>
-						<li class="AdminLinkBox"><a href="ChangeUser.php" class="AdminLink">Изменить/удалить пользователя</a></li>
-						<li class="AdminLinkBox"><a href="AddBookForm.php" class="AdminLink">Добавить книгу</a></li>
-						<li class="AdminLinkBox"><a href="ChangeBook.php" class="AdminLink">Изменить/удалить книгу</a></li>
-						<li class="AdminLinkBox"><a href="InfoAboutUsers.php" class="AdminLink">Информация о пользователях</a></li>
-						<li class="AdminLinkBox"><a href="InfoAboutBooks.php" class="AdminLink">Информация о книгах</a></li>
-						<li class="AdminLinkBox"><a href="CustomizeSlider.php" class="AdminLink">Настройка главной страницы</a></li>
-						<li class="AdminLinkBox"><a href="#" class="AdminLink" onclick="document.location.replace('?exit');">Выход</a></li>
-					</ul>
+				<? include_once "AdminNavigation.php"; ?>
 			</div>
 			<div class="Option">
 				<h2 class="MainHeader">Настройки слайдеров</h2>
